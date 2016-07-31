bbb9fc99778f5cc9505028fc40d4d75c94ef09b1
Maximilian Algehed <m.algehed@gmail.com>
2016-07-31 13:59:25 +0200

    Did some minor work on traces and generators for traces

--- SessionTypes.hs
+++ SessionTypes.hs
@@ -1,46 +1,73 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFunctor, FlexibleInstances, MultiParamTypeClasses #-}
 module SessionTypes where
+import Test.QuickCheck
+import Generator
 
-data SessionType t = B (SessionType t)
-                   | Q (SessionType t)
-                   | End
-                   | (SessionType t) :& (SessionType t)
-                   | (SessionType t) :| (SessionType t)
-                   | (SessionType t) :. (SessionType t)
-                   | Primitive t
+data SessionType t = B t -- Send something
+                   | Q t -- Get something
+                   | (SessionType t) :& (SessionType t) -- Branch
+                   | (SessionType t) :| (SessionType t) -- Choice
+                   | (SessionType t) :. (SessionType t) -- Composition
+                   | End -- Termination
                    deriving (Show, Eq, Functor)
 
-data Session typeReal typeTemplate = Channel (SessionType typeTemplate)
-                                   | P (Session typeReal)
-                                   | L (Session typeReal)
-                                   | R (Session typeReal)
-                                   | End
+data Session typeReal typeTemplate = P typeReal
+                                   | L (Session typeReal typeTemplate)
+                                   | R (Session typeReal typeTemplate)
                                    deriving (Show)
 
-data Trace t = Send t (Trace t)
-             | Recv (t -> Trace t)
+data Trace t = Send t (Maybe (Trace t))
+             | Recv (Either (Maybe (Trace t)) (Trace t, Trace t))
              | Terminate
-             | Failed
+             deriving (Show)
+
+instance Functor Trace where
+    fmap f (Send t m) = Send (f t) (fmap (fmap f) m)
+    fmap f (Recv (Left m)) = Recv (Left (fmap (fmap f) m))
+    fmap f (Recv (Right (t, t'))) = Recv (Right (fmap f t, fmap f t'))
+    fmap _ Terminate = Terminate
+
+appendTrace :: Trace t -> Trace t -> Trace t
+appendTrace Terminate _               = Terminate
+appendTrace (Send t Nothing) tr       = Send t $ Just tr
+appendTrace (Send t (Just tr)) tr'    = Send t $ Just $ tr `appendTrace` tr'
+appendTrace (Recv (Left (Just t))) tr = Recv $ Left $ Just $ t `appendTrace` tr
+appendTrace (Recv (Left Nothing)) tr  = Recv $ Left $ Just tr
+appendTrace (Recv (Right (t, t'))) tr = Recv $ Right (t `appendTrace` tr, t' `appendTrace` tr)
+
+-- | This function is buggy at the moment, it does not work branches
+implementTrace :: (Implements t t') => SessionType t' -> Gen (Trace (Session t t'))
+implementTrace (B t)       = do
+                                t' <- implement t
+                                return $ Send (P t') Nothing 
+implementTrace (Q t)       = return $ Recv $ Left Nothing
+implementTrace (st :| st') = oneof $ [fmap (fmap L) (implementTrace st), fmap (fmap R) (implementTrace st')]
+implementTrace (st :& st') = do
+                                t  <- implementTrace st
+                                t' <- implementTrace st'
+                                return $ Recv $ Right (t, t') 
+implementTrace (st :. st') = do
+                                t  <- implementTrace st
+                                t' <- implementTrace st'
+                                return $ t `appendTrace` t'
+implementTrace End         = return Terminate
+
+instance (Implements t t') => Implements (Trace (Session t t')) (SessionType t') where
+    implement = implementTrace
 
 dual :: SessionType t -> SessionType t
-dual (B t)     = Q (dual t)
-dual (Q t)     = B (dual t)
+dual (B t)     = Q t
+dual (Q t)     = B t
 dual (t :& t') = (dual t) :| (dual t')
 dual (t :| t') = (dual t) :& (dual t')
 dual (t :. t') = (dual t) :. (dual t')
 dual t         = t
 
-t :: t -> SessionType t
-t = Primitive
-
-v :: Name -> SessionType t
-v = Var
-
 end :: SessionType t
 end = End
 
-(!) :: SessionType t -> SessionType t
+(!) :: t -> SessionType t
 (!) = B
 
-(?) :: SessionType t -> SessionType t
+(?) :: t -> SessionType t
 (?) = Q
