d46175316ecc2f137a31917e41268a9a058c3c66
Maximilian Algehed <m.algehed@gmail.com>
2016-08-02 00:11:47 +0200

    IT WERKS EHRMARGHERDER!

--- src/Examples.hs
+++ src/Examples.hs
@@ -1,9 +1,15 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
 module Examples where
 
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Typeclasses
+import Model
 import Test.QuickCheck
-import SessionTypes
+import SessionTypes hiding (B)
 import JSONType
 import LTL
+import Run
 
 shopping :: SessionType JSONType
 shopping = addBook :. (shopping :| checkOut)
@@ -15,20 +21,72 @@ sizedShopping :: Int -> SessionType JSONType
 sizedShopping 0 = (!) JNumber :. end
 sizedShopping n = (!) JNumber :. (sizedShopping (n-1) :| (!) JNumber) :. end
 
--- | Check that once a book is ordered, it will show up in the next
--- | list of books
-data Shopping = Book Int | YourBasket [Int] | WhateverNonense deriving (Show)
 
-booksPredicate :: LTL Shopping
-booksPredicate = a (\message -> case message of
-                        Book b -> U (Not (a isBasket)) (a (contains b))
-                        _      -> Top
+{- What follows is a toy example worked out for the purpose of
+ - showing what is possible with the system. The example shows
+ - how we can specify the correctness of a shopping system -} 
+
+data Shopping = Book Int | YourBasket [Int] | RequestBasket deriving (Show)
+
+data ShoppingType = B | YB | RB deriving (Show)
+
+instance Implements Shopping ShoppingType where
+    implement B = fmap Book arbitrary
+    implement YB = fmap YourBasket arbitrary
+    implement RB = return RequestBasket
+
+instance Checks ShoppingType Shopping where
+    check B (Book _) = True
+    check YB (YourBasket _) = True
+    check RB RequestBasket = True
+    check _ _ = False
+
+-- | The session type for an interaction in our magical book store
+bookShopClient :: SessionType ShoppingType
+bookShopClient = (!)B :. (bookShopClient :| ((!)RB :. (?)YB)) :. end
+
+-- | A predicate that says that IF I add a book to the shopping basket,
+-- I will see that book in the basket when I request to see my basket
+booksPredicate :: LTL (Interaction (Protocol Shopping))
+booksPredicate = G $ a (\message -> case message of
+                        Sent (Pure (Book b)) -> U (Not (a isBasket)) (a (contains b))
+                        _            -> Top
                    )
                where
-                isBasket (YourBasket _) = Top
-                isBasket _              = Bottom
+                isBasket (Got (Pure (YourBasket _))) = Top
+                isBasket _                           = Bottom
 
-                contains b (YourBasket bs)
+                contains b (Got (Pure (YourBasket bs)))
                     | b `elem` bs       = Top
                     | otherwise         = Bottom
                 contains _ _            = Bottom
+
+bookServer :: (Chan (Protocol Shopping), Chan (Protocol Shopping)) -> IO ()
+bookServer (chi, cho) = loop []
+    where
+        loop xs = do
+                    (Pure (Book b)) <- readChan chi
+
+                    -- There is a bug RIGHT HERE
+                    -- for demonstration purposes!
+                    if b > 0 then
+                        awaitBranch (b:xs)
+                    else
+                        awaitBranch xs
+
+        awaitBranch xs = do
+                    br <- readChan chi
+                    case br of
+                        ChooseLeft -> loop xs
+                        ChooseRight -> do
+                                         Pure RequestBasket <- readChan chi
+                                         writeChan cho (Pure (YourBasket xs))
+                                         return ()
+                        _           -> return ()
+
+testBooks = do
+                ch <- newChan
+                ch' <- newChan
+                forkIO $ bookServer (ch, ch')
+                test bookShopClient (P ch' ch) booksPredicate
+
--- src/Run.hs
+++ src/Run.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE MultiParamTypeClasses, DeriveFunctor, FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses, DeriveFunctor, FlexibleContexts, FlexibleInstances #-}
 module Run where
+
 import Control.Monad.Writer.Lazy
 import Control.Concurrent.Chan
 import Test.QuickCheck
@@ -7,16 +8,22 @@ import Typeclasses
 import SessionTypes
 import JSONType
 import Model
+import qualified LTL as LTL
+
+data P a x = P (a x) (a x)
 
 class Channel ch t where
     new :: IO (ch (Protocol t))
     put :: ch (Protocol t) -> Protocol t -> IO ()
     get :: ch (Protocol t) -> IO (Protocol t)
 
-instance Channel Chan JSONValue where
-    new = newChan
-    put = writeChan
-    get = readChan
+instance Channel (P Chan) a where
+    new = do
+            cin <- newChan
+            cout <- newChan
+            return $ P cin cout
+    put (P cin cout) = writeChan cout
+    get (P cin cout) = readChan cin
 
 -- Checks that a Channel implements the protocol given by
 -- a session type by testing it against a random trace that conforms
@@ -49,3 +56,21 @@ checkProtocolCompliance st ch = lift (generate (implementTrace st)) >>= runHelpe
         runHelper (Choose L (Just tr)) = lift (put ch ChooseLeft) >> tell [(Sent ChooseLeft)] >> runHelper tr
         runHelper (Choose R (Just tr)) = lift (put ch ChooseRight) >> tell [(Sent ChooseRight)] >> runHelper tr
         runHelper _                    = return False
+
+-- | Test if a channel implements a session type
+-- | and a predicate in LTL holds for the interaction
+test :: (Implements r t, Checks t r, Channel ch r, Show r) =>
+    SessionType t ->
+    ch (Protocol r) ->
+    LTL.LTL (Interaction (Protocol r)) ->
+    IO Bool
+test t ch pred = do
+                    (b, logs) <- runWriterT (checkProtocolCompliance t ch)
+                    if not b then
+                        do
+                            putStrLn $ show logs
+                            return False
+                    else
+                        do
+                            putStrLn $ show logs
+                            return $ LTL.check pred logs
