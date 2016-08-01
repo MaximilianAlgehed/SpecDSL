e7e0681f06dfdf1025f965d7a94d370a164ba47d
Maximilian Algehed <m.algehed@gmail.com>
2016-08-01 19:23:45 +0200

    Added support for variable binding in LTL

--- src/Examples.hs
+++ src/Examples.hs
@@ -3,6 +3,7 @@ module Examples where
 import Test.QuickCheck
 import SessionTypes
 import JSONType
+import LTL
 
 shopping :: SessionType JSONType
 shopping = addBook :. (shopping :| checkOut)
@@ -13,3 +14,21 @@ shopping = addBook :. (shopping :| checkOut)
 sizedShopping :: Int -> SessionType JSONType
 sizedShopping 0 = (!) JNumber :. end
 sizedShopping n = (!) JNumber :. (sizedShopping (n-1) :| (!) JNumber) :. end
+
+-- | Check that once a book is ordered, it will show up in the next
+-- | list of books
+data Shopping = Book Int | YourBasket [Int] | WhateverNonense deriving (Show)
+
+booksPredicate :: LTL Shopping
+booksPredicate = a (\message -> case message of
+                        Book b -> U (Not (a isBasket)) (a (contains b))
+                        _      -> Top
+                   )
+               where
+                isBasket (YourBasket _) = Top
+                isBasket _              = Bottom
+
+                contains b (YourBasket bs)
+                    | b `elem` bs       = Top
+                    | otherwise         = Bottom
+                contains _ _            = Bottom
--- src/LTL.hs
+++ src/LTL.hs
@@ -2,7 +2,9 @@ module LTL where
 
 -- LTL defined by a set of adequate connectives
 -- This is the minimal definition of LTL
-data LTL domain = Atomic (domain -> Bool)
+data LTL domain = Atomic (domain -> LTL domain)
+                | Top
+                | Bottom
                 | Terminated
                 | And (LTL domain) (LTL domain)
                 | Not (LTL domain)
@@ -13,12 +15,14 @@ data LTL domain = Atomic (domain -> Bool)
 -- Check if an execution trace satisfies an LTL formula
 -- This needs to be extended to fit finite traces
 check :: LTL domain -> [domain] -> Bool
-check Terminated []    = True
-check (Atomic f) (x:_) = f x
-check (And l r) xs     = check l xs && check r xs
-check (Not ltl) xs     = not $ check ltl xs
-check (X ltl) (_:xs)   = check ltl xs
-check (U l r) xs       = case y of
+check Top        _      = True
+check Bottom     _      = False
+check Terminated []     = True
+check (Atomic f) (x:xs) = check (f x) xs
+check (And l r) xs      = check l xs && check r xs
+check (Not ltl) xs      = not $ check ltl xs
+check (X ltl) (_:xs)    = check ltl xs
+check (U l r) xs        = case y of
                             -- r eventually becomes true
                             Just y' -> and [check l (drop n xs) | n <- [0..y'-1]]
                             -- r never becomes true
@@ -27,10 +31,12 @@ check (U l r) xs       = case y of
         y = fir (Just 0) r xs
         fir n f [] = if check f [] then n else Nothing
         fir n f t  = if check f t then n else fir (fmap (+1) n) f (tail t)
-check (G ltl) xs       = and [check ltl (drop n xs) | n <- [0..(length xs - 1)]]
-check _ _              = False
+check (G ltl) xs        = and [check ltl (drop n xs) | n <- [0..(length xs - 1)]]
+check _ _               = False
 
 -- Some nice shorthands
 a .| b = Not (And (Not a) (Not b))
 (.&) = And
 a = Atomic
+iff p = Atomic (\x -> if p x then Top else Bottom)
+b ===> ltl = if b then ltl else Top
