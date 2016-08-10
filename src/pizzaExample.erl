44dcf88423fe2040117b45a489d42359ff51cd3a
Maximilian Algehed <m.algehed@gmail.com>
2016-08-10 23:55:13 +0200

    started working on the erlang pizza whatever...

--- src/pizzaExample.erl
+++ src/pizzaExample.erl
@@ -2,3 +2,22 @@
 -export(main).
 
 main() -> spawn(fun() -> register(p, self()), loop([]) end).
+
+loop(Items) ->
+    recieve
+        {Hs, {pure, hello}} -> Item = getItem(),
+                               recieve
+                                    {Hs, chooseLeft} -> loop([Item|Items]);
+                                    {Hs, chooseRight} -> finish(Hs, [Item|Items])
+                               end
+    end.
+
+finish(Hs, Items) ->
+    Hs ! Items,
+    Hs ! 1000,
+    recieve
+        _ -> pass
+    end,
+    recieve
+        _ -> pass
+    end.
